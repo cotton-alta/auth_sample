0d3ff43f896c827fb95adfdee97a471c80d95757
cotton <chinoknct@gmail.com>
2019-11-06 00:13:43 +0900

    突然だけど完成版

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>index page</h2>
+    <ul>
+      <li>
+        <nuxt-link to='/login'>ログインページへ</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to='/authed-route'>認証が必要なページへ</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
